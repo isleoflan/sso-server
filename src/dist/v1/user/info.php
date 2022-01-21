6fbe6597ebeabc68b23ac02a09e668af7fb72190
stui <me@stui.ch>
2022-01-21 14:39:41 +0100

    added hasOrder key

--- src/dist/v1/user/info.php
+++ src/dist/v1/user/info.php
@@ -17,4 +17,27 @@ $user = $response->check();
 
 $response->setData($user->serialize());
 
-$response->addData('hasOrder', true);
+
+$apiRequest = curl_init('https://api.shop.isleoflan.ch/v1/user/orderinfo?userId='.$user->getId());
+
+$headers = [
+    'Iol-App-Token: '.APIResponse::getRequestHeader('Iol-App-Token'),
+    'Content-Type: application/json',
+    'Authorization: '.APIResponse::getRequestHeader('Authorization'),
+];
+
+curl_setopt($apiRequest, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($apiRequest, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($apiRequest);
+$responseCode = curl_getinfo($apiRequest, CURLINFO_HTTP_CODE);
+
+$hasOrder = false;
+if($responseCode == 200){
+    $response = json_decode($response, true);
+
+    if(!isset($response['error'])) {
+        $hasOrder = $response['data']['hasOrder'];
+    }
+}
+
+$response->addData('hasOrder', $hasOrder);
